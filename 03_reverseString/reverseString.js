3bf9cb80c1006bf73ce74a960f7d360e72067d1e
spectre729 <dchen4761@gmail.com>
2025-01-17 15:19:52 -0500

    Completed the first 7 javascript exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = "";
+    let resultArray = [];
+    let counter = -1;
+    for (let i = 0; i < str.length; i++){
+        resultArray[i] = str.at(counter);
+        counter--;
+        result += resultArray[i];
+    }
+    return result;
+    
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if ((!(Number.isInteger(a) && Number.isInteger(b))) || a < 0 || b < 0){
+        return "ERROR";
+    }
+    let sum = 0;
+    if (a > b){
+        for (let i = b; i <=a ; i++){
+            sum += i;
+        }
+    }
+    else{
+        for (let i = a; i <= b; i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
